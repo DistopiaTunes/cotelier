11cc65d870328f251bde1744d70ac951f45d0134
DistopiaTunes <167197649+DistopiaTunes@users.noreply.github.com>
2025-06-18 22:01:46 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Cotelier from './components/Cotelier.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 </script>
 
 <template>
-<Cotelier/>
+  <el-config-provider>
+    <Cotelier />
+  </el-config-provider>
 </template>
 
-<style scoped>
-
+<style>
+/* Add any global styles here */
 </style>
