df677efe991b7bfcdca9bc55a95270ee0d64fbb6
DistopiaTunes <distopiatunes@gmail.com>
2025-12-11 16:45:20 -0300

    Ajustes  dados equipe

--- src/components/Cotelier.vue
+++ src/components/Cotelier.vue
@@ -121,8 +121,8 @@ const teachers = {
     },
     2: {
         name: 'Raphael Francisco', 
-        role: 'Artista multidisciplinar',
-        bio: 'Raphael explora constantemente novas linguagens nas artes visuais e dedica-se a compartilhar seu saber e experiência artística com a comunidade.',
+        role: 'Artista multidisciplinar e Professor',
+        bio: 'Raphael é mestre em pintura pela UNESPAR, atua há mais de 10 anos explorando, experimentando e ensinando diversas técnicas e materiais nas artes visuais. Sua abordagem une fundamentos clássicos a práticas atuais, com foco em inovação e investigação artística.',
         fullBio: 'Raphael é mestre em pintura pela UNESPAR, com mais de 10 anos de atuação na prática e ensino das artes visuais. Sua abordagem destaca a experimentação e o domínio de diferentes técnicas e materiais, buscando conciliar fundamentos clássicos com práticas artísticas atuais. Está sempre em busca de aprimoramento e inovação, trazendo uma visão ousada e investigativa para suas obras, especialmente quando trabalha com abstração, arte digital e pesquisas em linguagens visuais emergentes.',
         image: 'https://raw.githubusercontent.com/DistopiaTunes/cotelier/refs/heads/main/src/assets/img/profs/raphael.jpg',
         instagram: 'https://instagram.com/raphael.ars',
@@ -131,7 +131,7 @@ const teachers = {
     3: {
         name: 'Mônica Lara',
         role: 'Artista plástica, Tatuadora e Professora',
-        bio: 'Mônica Lara é uma artista plástica, tatuadora e professora de artes visuais. Sua abordagem é contemporânea e inovadora...',
+        bio: 'Mônica Lara é uma artista plástica, tatuadora e professora de artes visuais. Sua abordagem é contemporânea e inovadora, sempre buscando novas formas de inspirar seus alunos. Possui experiência com diversas linguagens, técnicas mistas e projetos artísticos autorais, conectando criatividade e sensibilidade ao estimular o desenvolvimento pessoal por meio da arte.',
         fullBio: 'Especializada em conduzir processos criativos com o público 60+.  Monica é focada em promover expressão, autonomia e bem-estar por meio da arte, sempre valorizando as histórias, vivências e identidades de cada pessoa. Desenvolve oficinas e aulas de pintura e desenho  que exploram pintura, colagem, técnicas manuais e experimentações sensoriais, criando ambientes acolhedores onde cada participante se sente confiante para descobrir novas formas de se expressar. "Acredito profundamente no poder da arte como ferramenta de conexão, autoestima e saúde emocional — especialmente na maturidade. Meu propósito é transformar cada encontro em uma experiência única, significativa e cheia de cores"',
         image: 'https://raw.githubusercontent.com/DistopiaTunes/cotelier/refs/heads/main/src/assets/img/profs/monica.jpg',
         instagram: 'https://instagram.com/monicatatuadora',
@@ -140,7 +140,7 @@ const teachers = {
 
     4: {
         name: 'Carlos Alexandre',
-        role: 'Desenhista, Ilustrador eProfessor',
+        role: 'Desenhista, Ilustrador e Professor',
         bio: 'Carlos Alexandre tem sólida experiência em desenho, ilustração digital e tradicional, quadrinhos e mangás. Já ilustrou diversos livros e projetos editoriais. Domina técnicas variadas e incentiva criatividade e expressão em seus alunos.',
         fullBio: 'Carlos Alexandre atua há 10 anos nas artes visuais, com experiência em desenho, ilustração digital e tradicional, quadrinhos, além de adicionar ilustração de mangás e animes ao seu repertório artístico. Já ilustrou mais de dez livros infantis — vários publicados — além de revistas, capas, mascotes e projetos editoriais. Domina ferramentas como Photoshop e Illustrator, criando personagens, narrativas visuais e artes em diversos estilos. Também possui prática em retratos realistas, aquarela e lápis de cor. Como professor, dedica-se à formação de novos artistas, incentivando criatividade, técnica e expressão pessoal.',
         image: 'https://raw.githubusercontent.com/DistopiaTunes/cotelier/refs/heads/main/src/assets/img/profs/alex.jpg',
@@ -808,7 +808,7 @@ onUnmounted(() => {
                     <img :src="teacher.image" :alt="teacher.name" class="w-48 h-48 rounded-full mx-auto mb-6 object-cover hover:opacity-90 transition-opacity">
                     <h3 class="font-lora text-2xl text-stone-800 mb-3">{{ teacher.name }}</h3>
                     <p class="font-lora text-xl text-stone-600 mb-4">{{ teacher.role }}</p>
-                    <p class="font-lora text-lg text-stone-500 mb-2">{{ teacher.bio }}</p>
+                    <p class="font-lora text-lg text-stone-500 mb-2 text-justify">{{ teacher.bio }}</p>
                     <button @click="showTeacherBio(id)" class="text-blue-600 hover:text-blue-800 font-lora text-lg">Ler mais</button>
                 </div>
             </div>
@@ -829,7 +829,7 @@ onUnmounted(() => {
                     <img :src="currentTeacher?.image" :alt="currentTeacher?.name" class="w-48 h-48 rounded-full object-cover mx-auto md:mx-0">
                     <div>
                         <p class="font-cormorant text-xl text-stone-600 mb-4">{{ currentTeacher?.role }}</p>
-                        <p class="font-cormorant text-lg text-stone-500 mb-6">{{ currentTeacher?.fullBio }}</p>
+                        <p class="font-cormorant text-lg text-stone-500 mb-6 text-justify">{{ currentTeacher?.fullBio }}</p>
                         <a :href="currentTeacher?.instagram" target="_blank" class="bg-pink-600/80 hover:bg-pink-700/80 text-white px-4 py-2 rounded-lg flex items-center justify-center text-sm w-fit">
                             <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
