37bb3bcb76c56dc13fba9dfccd428eae0acae1f3
DistopiaTunes <distopiatunes@gmail.com>
2025-12-11 09:53:11 -0300

    2 - workshop ceramica

--- src/components/Cotelier.vue
+++ src/components/Cotelier.vue
@@ -54,12 +54,25 @@ const workshops = [
     {
         title: 'Cerâmica - Pratinhos & Potinhos',
         image: 'https://raw.githubusercontent.com/DistopiaTunes/cotelier/refs/heads/main/src/assets/img/eventos/cer/prato.jpg',
-        description: 'Aprenda as técnicas fundamentais da pintura em aquarela com nossa oficina especializada.',
-        details: 'Durante 4 encontros, vamos explorar técnicas artísticas de forma leve, divertida e super criativa: Pintura com giz pastel oleos, Mosaico com aquarela, Colagem usando papelão, Artesanato com papel alumínio e canetinhas',
-        additionalInfo: 'Um convite para experimentar, ousar, imaginar e transformar ideias em arte. Materiais inclusos. Vagas limitadas!',
+        description: 'Aprenda cerâmica de forma prática e divertida.',
+        details: 'Oficina para iniciantes que querem viver a experiência da cerâmica. Iremos aprender a modelar as peças pela técnica de abertura de placas e pinch alémn de  explorar diversas possibilidades na criação e personalização. Faremos a festa usando carimbos e cortadores.',
+        additionalInfo: 'Oportunidade imperdível! Materiais inclusos. Vagas limitadas!',
         whatsappMessage: 'Olá! Gostaria de informações sobre a Oficina de Pintura em Aquarela',
-        teacherIds: [5],
-        price: '$100',
+        teacherIds: [6],
+        price: '$250',
+        dateTime: '15 Jan - 14h',
+        instagramLink: 'https://instagram.com/cotelier.oficial'
+    },
+
+    {
+        title: 'Cerâmica - Xícara e Bowl com Vinho',
+        image: 'https://raw.githubusercontent.com/DistopiaTunes/cotelier/refs/heads/main/src/assets/img/eventos/cer/xic.jpeg',
+        description: 'Aprenda cerâmica de forma prática e divertida.',
+        details: 'Oficina para iniciantes que querem viver a experiência da cerâmica. Iremos aprender a modelar as peças pela técnica de abertura de placas e pinch alémn de  explorar diversas possibilidades na criação e personalização. Faremos a festa usando carimbos e cortadores.',
+        additionalInfo: 'Oportunidade imperdível! Materiais inclusos. Vagas limitadas!',
+        whatsappMessage: 'Olá! Gostaria de informações sobre a Oficina de Pintura em Aquarela',
+        teacherIds: [6],
+        price: '$250',
         dateTime: '15 Jan - 14h',
         instagramLink: 'https://instagram.com/cotelier.oficial'
     },
@@ -144,6 +157,7 @@ const teachers = {
         role: 'Ceramista e Psicóloga',
         bio: 'Luciana Kotaka é ceramista e psicóloga, unindo as duas áreas em um trabalho que visa produzir peças que revelam nossa essência. A cerâmica possibilita um trabalho interno intenso, pois o processo entre a criação e a finalização das peças nos leva a confrontar aspectos como o tempo, o perfeccionismo, o estado de presença e a frustração. Um trabalho artístico e emocional.',
         fullBio:'Luciana Kotaka é ceramista e psicóloga, unindo as duas áreas em um trabalho que visa produzir peças que revelam nossa essência. A cerâmica possibilita um trabalho interno intenso, pois o processo entre a criação e a finalização das peças nos leva a confrontar aspectos como o tempo, o perfeccionismo, o estado de presença e a frustração. Um trabalho artístico e emocional.',
+        image: 'https://raw.githubusercontent.com/DistopiaTunes/cotelier/refs/heads/main/src/assets/img/profs/luciana.jpeg',
         instagram: 'https://instagram.com/lu.ceramicando',
         instagramLabel: '@lu.ceramicando'
     }
@@ -754,6 +768,34 @@ onUnmounted(() => {
         <div class="max-w-4xl mx-auto">
             <h2 class="font-lora text-4xl md:text-5xl text-center mb-16 text-stone-800">Horários de Aulas</h2>
             <div class="space-y-8">
+
+                <p class="text-2xl text-stone-700 text-center mb-10 font-lora">Segunda-feira</p>
+                
+                
+                <div class="bg-white p-8 rounded-lg shadow-sm">
+                    <div class="flex items-center justify-between mb-4">
+                        <h3 class="font-lora text-2xl text-stone-800">Oficina Livre de Desenho Artístico e Pintura em Tela</h3>
+                        <div class="flex items-center gap-1">
+                            <svg class="w-6 h-6 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                            <span class="text-blue-500 text-sm font-medium">Disponível</span>
+                        </div>
+                    </div>
+                    <p class="font-lora text-xl text-stone-600 mb-4">Terças-feiras das <b><u>14:00 às 15:30h</u></b> e <b><u>16:00 às 17:30h</u></b>- Faixa etária: a partir de 7 anos</p>
+                    <p class="font-lora text-lg text-stone-500 mb-4">Perfeito para quem está começando sua jornada artística. Aprenda técnicas fundamentais de desenho e pintura, luz e sombra, proporção, teoria das cores e mais.</p>
+                    <div class="flex gap-4">
+                        <a href="https://wa.me/554130492413?text=Olá! Gostaria de informações sobre o curso de Pintura a Óleo para iniciantes" target="_blank" class="bg-green-500/80 hover:bg-green-600/80 text-white px-4 py-2 rounded-lg flex items-center justify-center text-sm w-fit">
+                            <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"/>
+                            </svg>
+                            Agende uma aula experimental
+                        </a>
+                    </div>
+                </div>
+
+
+                <p class="text-2xl text-stone-700 text-center mb-10 font-lora">Terça-feira</p>
                 
                 
                 <div class="bg-white p-8 rounded-lg shadow-sm">
